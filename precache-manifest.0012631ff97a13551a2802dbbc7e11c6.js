45fc80fc5f2dbe88343796bec76013f2615a3fd3
Austen Young <austen.d.young@gmail.com>
2020-03-22 14:44:13 -0400

    Updates

---
+++ precache-manifest.0012631ff97a13551a2802dbbc7e11c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2d00e191d583ef2726a2119eb7290f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bf6a22dc3fa9296c89c5",
+    "url": "./static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "d380baa5a5db63eca2cd",
+    "url": "./static/css/main.1cce2c20.chunk.css"
+  },
+  {
+    "revision": "bf6a22dc3fa9296c89c5",
+    "url": "./static/js/2.6877d6d3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.6877d6d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d380baa5a5db63eca2cd",
+    "url": "./static/js/main.074bc1b5.chunk.js"
+  },
+  {
+    "revision": "7ca914a5135d40d2e0d3",
+    "url": "./static/js/runtime-main.99bf5a14.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "./static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c59c232d6e9f678612e638b51ae54339.js"
+  "./precache-manifest.0012631ff97a13551a2802dbbc7e11c6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
